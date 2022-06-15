7b579f77447f1b2f5dd58e3d7c23d6f2ab2e04d5
Alexei Mamani <alexeim763@gmail.com>
2022-06-15 14:25:58 -0500

    Add images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `Learning Gatsby 4`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 }
